1ecbc6de79009385eaec82014f5c1cd60b1d646a
gharrels <43727098+gharrels@users.noreply.github.com>
2018-09-30 17:09:35 -0400

    Update test_that.R

--- tests/test_that.R
+++ tests/test_that.R
@@ -1,10 +1,6 @@
 require(testthat)
 library(FARS)
 
-test_that("fars_read returns a data frame with valid input", {
-  expect_that(fars_read(make_filename(2013)), is_a("data.frame"))
-})
-
 test_that("fars_read throws an error with invalid input", {
   expect_that(fars_read(make_filename(9999)), throws_error())
 })
